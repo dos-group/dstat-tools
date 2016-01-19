3f76f645ec4aa3084fda505c7e2b0084843ab69e
joh-mue <yesiamkeen@gmail.com>
2016-01-19 22:37:24 +0100

    spec template

--- spec/dstat_plot_spec.rb
+++ spec/dstat_plot_spec.rb
@@ -1,1 +1,22 @@
-dstat_plot_spec.rb
+# dstat_plot_spec.rb
+
+require 'dstat_plot'
+
+RSpec.describe 'dstat_plot' do
+  describe "#testfunction" do
+    it "multiplies input value should by 9" do
+      result = testfunction 5
+      expect(result).to eq(45)
+    end
+
+    it "nummer2" do
+
+    end
+  end
+
+  describe "#methode2" do
+    it "dsafsa" do
+      
+    end
+  end
+end
